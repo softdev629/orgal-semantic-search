e61eb8313ea94540694522277449ec8a36620ce6
Eugine <121113067+softdev629@users.noreply.github.com>
2024-05-21 22:52:21 +0300

    port change

--- main.py
+++ main.py
@@ -375,4 +375,4 @@ def get_credentials(website: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
